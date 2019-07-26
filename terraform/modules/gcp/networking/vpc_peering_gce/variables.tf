f0368bd121326149a9551358afb4ede801f6ddbf
Dmitriy Karbyshev <-z>
2019-07-26 03:16:56 +0300

    [#558] Add GCE VPC peering

---
+++ terraform/modules/gcp/networking/vpc_peering_gce/variables.tf
@@ -0,0 +1,29 @@
+variable "project_id" {
+  description = "Target project id"
+}
+
+variable "zone" {
+  description = "Default zone"
+}
+
+variable "region" {
+  description = "Region of resources"
+}
+
+variable "gcp_network_1_name" {
+  description = "VPC Network 1 name to peer with"
+}
+
+variable "gcp_network_1_range" {
+  type        = "list"
+  description = "VPC Network 1 range to allow"
+}
+
+variable "gcp_network_2_name" {
+  description = "VPC Network 2 name to peer with"
+}
+
+variable "gcp_network_2_range" {
+  type        = "list"
+  description = "VPC Network 2 range to allow"
+}
