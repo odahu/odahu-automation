4c27e91d66b2e2369134a553baaa2f822031b242
Maxim Kochubey <maxim_kochubey@epam.com>
2019-12-10 15:35:13 +0300

    Some providers nailed along the way

--- terraform/env_types/aws/eks/eks_create/provider.tf
+++ terraform/env_types/aws/eks/eks_create/provider.tf
@@ -2,3 +2,15 @@ provider "aws" {
   version = "2.33.0"
   region  = var.aws_region
 }
+
+provider "local" {
+  version = "~> 1.4"
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "template" {
+  version = "~> 2.1"
+}
