fff7f3151d041cf17c117a34461367e81b44068a
Dmitriy Karbyshev <-z>
2019-12-14 00:17:42 +0300

    [#957] Avoid name length limit

--- terraform/modules/gcp/networking/vpc_peering_gcp/main.tf
+++ terraform/modules/gcp/networking/vpc_peering_gcp/main.tf
@@ -12,14 +12,14 @@ data "google_compute_network" "gcp_network_2" {
 
 resource "google_compute_network_peering" "peering1" {
   count        = var.infra_vpc_peering
-  name         = "${data.google_compute_network.gcp_network_2[0].name}-${data.google_compute_network.gcp_network_1[0].name}-peering"
+  name         = substr("${data.google_compute_network.gcp_network_2[0].name}-${data.google_compute_network.gcp_network_1[0].name}-peering", 0, 62)
   network      = data.google_compute_network.gcp_network_1[0].self_link
   peer_network = data.google_compute_network.gcp_network_2[0].self_link
 }
 
 resource "google_compute_network_peering" "peering2" {
   count        = var.infra_vpc_peering
-  name         = "${data.google_compute_network.gcp_network_1[0].name}-${data.google_compute_network.gcp_network_2[0].name}-peering"
+  name         = substr("${data.google_compute_network.gcp_network_1[0].name}-${data.google_compute_network.gcp_network_2[0].name}-peering", 0, 62)
   network      = data.google_compute_network.gcp_network_2[0].self_link
   peer_network = data.google_compute_network.gcp_network_1[0].self_link
   depends_on   = [google_compute_network_peering.peering1]
@@ -48,7 +48,7 @@ resource "google_compute_firewall" "to_network_1" {
 resource "google_compute_firewall" "to_network_2" {
   count         = var.infra_vpc_peering
   project       = var.project_id
-  name          = "${data.google_compute_network.gcp_network_1[0].name}-${data.google_compute_network.gcp_network_2[0].name}"
+  name          = substr("${data.google_compute_network.gcp_network_1[0].name}-${data.google_compute_network.gcp_network_2[0].name}", 0, 62)
   network       = data.google_compute_network.gcp_network_1[0].name
   source_ranges = var.gcp_network_2_range
 
