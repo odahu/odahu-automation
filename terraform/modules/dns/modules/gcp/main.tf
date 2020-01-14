e745dc2b16643feda47c0310037fa40d3b2715bc
Dmitriy Karbyshev <-z>
2020-01-14 23:34:17 +0300

    [#51] wait while DNS become available to ensure that DNS configurated properly

--- terraform/modules/dns/modules/gcp/main.tf
+++ terraform/modules/dns/modules/gcp/main.tf
@@ -6,7 +6,8 @@ locals {
   root_domain  = lookup(local.parsed, "root_domain", "")
   managed_zone = length(var.managed_zone) == 0 ? google_dns_managed_zone.this[0].name : data.google_dns_managed_zone.this[0].name
   domain       = length(var.managed_zone) == 0 ? google_dns_managed_zone.this[0].dns_name : data.google_dns_managed_zone.this[0].dns_name
-  records      = { for rec in var.records : md5("${rec.name}_${rec.value}") => rec}
+  records      = { for rec in var.records : md5("${rec.name}_${rec.value}") => rec }
+  records_str  = join(" ", [ for rec in var.records : rec.name ])
 }
 
 resource "google_dns_managed_zone" "this" {
@@ -35,3 +36,14 @@ resource "google_dns_record_set" "this" {
   managed_zone = local.managed_zone
   rrdatas      = [lookup(each.value, "value")]
 }
+
+resource "null_resource" "wait_for_dns" {
+  triggers = {
+    build_number = timestamp()
+  }
+  provisioner "local-exec" {
+    command = "timeout 600 bash -c 'for NAME in ${local.records_str}; do until dig +short $NAME.${local.domain}; do sleep 5; done ; done'"
+  }
+  depends_on = [google_dns_record_set.this]
+}
+
