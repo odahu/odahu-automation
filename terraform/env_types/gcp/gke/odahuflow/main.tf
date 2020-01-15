bfb49ab374857b6f3d712bb0f3b7d91c98638b25
Dmitriy Karbyshev <-z>
2020-01-15 16:42:38 +0300

    [#51] Use static node pools names for different purposes

--- terraform/env_types/gcp/gke/odahuflow/main.tf
+++ terraform/env_types/gcp/gke/odahuflow/main.tf
@@ -2,10 +2,6 @@
 # Odahuflow setup
 ########################################################
 
-locals {
-  pools  = {for pool in keys(var.node_pools) :  lookup(lookup(var.node_pools[pool], "labels", {}), "mode", "") => var.node_pools[pool] if lookup(lookup(var.node_pools[pool], "labels", {}), "mode", "") != "" }
-}
-
 module "odahuflow_prereqs" {
   source       = "../../../../modules/odahuflow/prereqs/gke"
   project_id   = var.project_id
@@ -46,9 +42,9 @@ module "odahuflow_helm" {
   tls_secret_key = var.tls_key
   cluster_domain = "odahu.${var.cluster_name}.${var.root_domain}"
 
-  model_training_nodes   = contains(keys(local.pools), "odahu-flow-training") ? { node_selector = { mode = local.pools["odahu-flow-training"].labels["mode"] }, toleration = { Key = local.pools["odahu-flow-training"].taints[0].key, Operator = "Equal", Value = local.pools["odahu-flow-training"].taints[0].value, Effect = replace(title(lower(replace(local.pools["odahu-flow-training"].taints[0].effect, "_", " "))), " ", "") }} : { node_selector = null, toleration = null }
-  model_packaging_nodes  = contains(keys(local.pools), "odahu-flow-packaging") ? { node_selector = { mode = local.pools["odahu-flow-packaging"].labels["mode"] }, toleration = { Key = local.pools["odahu-flow-packaging"].taints[0].key, Operator = "Equal", Value = local.pools["odahu-flow-packaging"].taints[0].value, Effect = replace(title(lower(replace(local.pools["odahu-flow-packaging"].taints[0].effect, "_", " "))), " ", "") }} : { node_selector = null, toleration = null }
-  model_deployment_nodes = contains(keys(local.pools), "odahu-flow-deployment") ? { node_selector = { mode = local.pools["odahu-flow-deployment"].labels["mode"] }, toleration = { Key = local.pools["odahu-flow-deployment"].taints[0].key, Operator = "Equal", Value = local.pools["odahu-flow-deployment"].taints[0].value, Effect = replace(title(lower(replace(local.pools["odahu-flow-deployment"].taints[0].effect, "_", " "))), " ", "") }} : { node_selector = null, toleration = null }
+  model_training_nodes   = contains(keys(var.node_pools), "training") ? { node_selector = { mode = var.node_pools["training"].labels["mode"] }, toleration = { Key = var.node_pools["training"].taints[0].key, Operator = "Equal", Value = var.node_pools["training"].taints[0].value, Effect = replace(title(lower(replace(var.node_pools["training"].taints[0].effect, "_", " "))), " ", "") }} : { node_selector = null, toleration = null }
+  model_packaging_nodes  = contains(keys(var.node_pools), "packaging") ? { node_selector = { mode = var.node_pools["packaging"].labels["mode"] }, toleration = { Key = var.node_pools["packaging"].taints[0].key, Operator = "Equal", Value = var.node_pools["packaging"].taints[0].value, Effect = replace(title(lower(replace(var.node_pools["packaging"].taints[0].effect, "_", " "))), " ", "") }} : { node_selector = null, toleration = null }
+  model_deployment_nodes = contains(keys(var.node_pools), "model_deployment") ? { node_selector = { mode = var.node_pools["model_deployment"].labels["mode"] }, toleration = { Key = var.node_pools["model_deployment"].taints[0].key, Operator = "Equal", Value = var.node_pools["model_deployment"].taints[0].value, Effect = replace(title(lower(replace(var.node_pools["model_deployment"].taints[0].effect, "_", " "))), " ", "") }} : { node_selector = null, toleration = null }
 
   helm_repo                = var.helm_repo
   docker_repo              = var.docker_repo
