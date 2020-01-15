9f3c17cf475f0563e0391c9f87b16edfeb791144
Dmitriy Karbyshev <-z>
2020-01-15 18:15:38 +0300

    [#57] Wait while DNS become available when creating records

--- terraform/modules/dns/modules/gcp/main.tf
+++ terraform/modules/dns/modules/gcp/main.tf
@@ -7,7 +7,7 @@ locals {
   managed_zone = length(var.managed_zone) == 0 ? google_dns_managed_zone.this[0].name : data.google_dns_managed_zone.this[0].name
   domain       = length(var.managed_zone) == 0 ? google_dns_managed_zone.this[0].dns_name : data.google_dns_managed_zone.this[0].dns_name
   records      = { for rec in var.records : md5("${rec.name}_${rec.value}") => rec }
-  records_str  = join(" ", [ for rec in var.records : rec.name ])
+  records_str  = join(" ", [ for rec in var.records : "${rec.name}:${rec.value}" ])
 }
 
 resource "google_dns_managed_zone" "this" {
@@ -38,10 +38,11 @@ resource "google_dns_record_set" "this" {
 }
 
 resource "null_resource" "wait_for_dns" {
+   triggers = {
      build_number = timestamp()
    }
    provisioner "local-exec" {
-    command = "timeout 600 bash -c 'for NAME in ${local.records_str}; do until dig +short $NAME.${local.domain}; do sleep 5; done ; done'"
+    command = "timeout 600 bash -c 'for REC in ${local.records_str}; do NAME=$(echo $REC | cut -d':' -f1); VALUE=$(echo $REC | cut -d':' -f2); until [[ $(dig +short $NAME.${local.domain} | tr -d '\\n') == $VALUE ]]; do sleep 5; done ; done'"
    }
    depends_on = [google_dns_record_set.this]
  }
