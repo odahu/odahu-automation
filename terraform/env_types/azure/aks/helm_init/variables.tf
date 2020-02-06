873b4eaafa98ccd3b709e56d4b6e6db444d26e84
Maxim Kochubey <maxim_kochubey@epam.com>
2020-02-06 23:08:52 +0300

    [legion-cicd#49] Explicit Istio Helm repo definition

--- terraform/env_types/azure/aks/helm_init/variables.tf
+++ terraform/env_types/azure/aks/helm_init/variables.tf
@@ -12,15 +12,6 @@ variable "cluster_name" {
   default     = "odahuflow"
 }
 
-variable "tiller_image" {
-  default = "gcr.io/kubernetes-helm/tiller:v2.14.3"
-}
-
 variable "helm_repo" {
   description = "Odahuflow helm repo"
-}
-
-variable "istio_helm_repo" {
-  default     = "https://storage.googleapis.com/istio-release/releases/1.4.3/charts"
-  description = "Istio helm repo"
 }
--- terraform/env_types/gcp/gke/helm_init/main.tf
+++ terraform/env_types/gcp/gke/helm_init/main.tf
@@ -2,8 +2,6 @@
 # HELM Init
 ########################################################
 module "helm_init" {
-  source          = "../../../../modules/helm_init"
-  helm_repo       = var.helm_repo
-  istio_helm_repo = var.istio_helm_repo
-  tiller_image    = var.tiller_image
+  source    = "../../../../modules/helm_init"
+  helm_repo = var.helm_repo
 }
