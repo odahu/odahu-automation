982c76e7b5419422d56eb68553a28276bf10a084
Maxim Kochubey <maxim_kochubey@epam.com>
2020-03-20 09:33:22 +0000

    [legion-cicd#114] EKS network stuff cleanup
    * AWS region returned back as script parameter

--- scripts/aws_eni_cleanup.sh
+++ scripts/aws_eni_cleanup.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 CLUSTER_NAME=$1
+REGION=$2
 
-ENI_LIST=$(aws ec2 describe-network-interfaces --filters="Name=group-name,Values=tf-${CLUSTER_NAME}-node" --output text --query 'NetworkInterfaces[*]'.['NetworkInterfaceId'])
+ENI_LIST=$(aws ec2 describe-network-interfaces --region "${REGION}" --filters="Name=group-name,Values=tf-${CLUSTER_NAME}-node" --output text --query 'NetworkInterfaces[*]'.['NetworkInterfaceId'])
 
 for eni in ${ENI_LIST}; do
-  aws ec2 delete-network-interface --network-interface-id "$eni"
+  aws ec2 delete-network-interface --region "${REGION}" --network-interface-id "$eni" && echo "Deleted ${eni}"
 done
