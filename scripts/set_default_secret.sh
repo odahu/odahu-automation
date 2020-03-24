086a4c698ebc5fc8ad3747f59ec7d4bc83fd0232
Maxim Kochubey <maxim_kochubey@epam.com>
2020-03-24 10:43:22 +0000

    [fix] Conditional docker_auth
    * Added conditional to avoid adding empty `dockerconfigjson`
    * `set_default_secret.sh` moved to common `scripts` directory

---
+++ scripts/set_default_secret.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+SECRET_NAME=$1
+NS=$2
+SA_LIST=$3
+for SA in $SA_LIST
+do
+  kubectl patch serviceaccount -n "$NS" "$SA" -p "{\"imagePullSecrets\": [{\"name\": \"${SECRET_NAME}\"}]}"
+done
