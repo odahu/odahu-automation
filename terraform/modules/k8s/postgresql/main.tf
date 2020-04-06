2c79c043f2f8f7d4f003b022bd00bb31d5c1f5b7
Dmitriy Karbyshev <-z>
2020-04-07 01:03:32 +0300

    [#65] sync with git-sync repo

--- terraform/modules/k8s/postgresql/main.tf
+++ terraform/modules/k8s/postgresql/main.tf
@@ -1,14 +1,13 @@
 locals {
-  pg_version          = "11.7.0-debian-10-r51"
-  pg_registry         = "gcr.io"
-  pg_repository       = "or2-msq-epmd-legn-t1iylu/odahu/postgresql-repmgr"
+  pg_version          = "11.7.0-debian-10-r13"
   debug_log_level     = "true"
   helm_repo           = "bitnami"
-  helm_version        = "2.0.1"
+  helm_version        = "1.4.6"
   deploy_helm_timeout = "600"
 }
 
 resource "kubernetes_namespace" "this" {
+  count = var.configuration.enabled ? 1 : 0
   metadata {
     annotations = {
       name = var.namespace
@@ -22,14 +21,12 @@ resource "helm_release" "this" {
   name       = "db"
   chart      = "postgresql-ha"
   version    = local.helm_version
-  namespace  = kubernetes_namespace.this.metadata[0].annotations.name
+  namespace  = var.namespace
   repository = local.helm_repo
   timeout    = local.deploy_helm_timeout
 
   values = [
     templatefile("${path.module}/templates/values.yaml", {
-      pg_registry      = local.pg_registry
-      pg_repository    = local.pg_repository
       password         = var.configuration.password
       storage_size     = var.configuration.storage_size
       debug            = local.debug_log_level
