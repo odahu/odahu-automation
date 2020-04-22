cf207ef0d15999adc4b4f7627ee86c9e9dd97913
Dmitriy Karbyshev <-z>
2020-04-23 02:29:23 +0300

    [#95] fix module params

--- terraform/modules/k8s/fluentd-daemonset/main.tf
+++ terraform/modules/k8s/fluentd-daemonset/main.tf
@@ -17,16 +17,15 @@ module "docker_credentials" {
 
 resource "helm_release" "fluentd-daemonset" {
   name       = "logging-operator"
-  chart      = local.chart
-  version    = local.helm_version
-  namespace  = "logging"
-  repository = local.helm_repo
+  chart      = "odahu-flow-fluentd-daemonset"
+  version    = var.odahu_infra_version
+  namespace  = var.namespace
+  repository = "odahuflow"
 
   values = [
     templatefile("${path.module}/templates/helm_values.yaml", {
       docker_repo         = var.docker_repo
-      odahu_infra_version = "1.2.0-b1587551770858"
-#var.odahu_infra_version
+      odahu_infra_version = var.odahu_infra_version
     }),
     var.extra_helm_values
   ]
