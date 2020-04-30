e99ac06bcd3c88923fa7860bc166c9cadafe1e4c
Maxim Kochubey <maxim_kochubey@epam.com>
2020-04-30 10:42:27 +0300

    [legion-cicd#165] Tolerations values added to `gke-saa` daemonset

--- terraform/modules/k8s/gke-saa/variables.tf
+++ terraform/modules/k8s/gke-saa/variables.tf
@@ -17,8 +17,15 @@ variable "cluster_type" {
 # GKE Service Account Assigner
 ########################
 variable "gke_saa_default_scopes" {
-  default     = "https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append"
+  default = [
+    "https://www.googleapis.com/auth/logging.write",
+    "https://www.googleapis.com/auth/monitoring",
+    "https://www.googleapis.com/auth/servicecontrol",
+    "https://www.googleapis.com/auth/service.management.readonly",
+    "https://www.googleapis.com/auth/trace.append"
+  ]
   description = "Default scopes to provide for pods"
+  type        = list(string)
 }
 
 variable "gke_saa_default_sa" {
