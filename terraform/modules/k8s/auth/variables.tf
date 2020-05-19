c3c98da3c23aa9b581a52c3f414997e35bc6757f
Maxim Kochubey <maxim_kochubey@epam.com>
2020-05-19 12:14:42 +0300

    [fix] `oauth2-proxy` image updated to recent version
    For details see https://github.com/oauth2-proxy/oauth2-proxy/security/advisories/GHSA-j7px-6hwj-hpjg
    - image version updated to v5.1.1
    - chart version updated to 3.1.0

--- terraform/modules/k8s/auth/variables.tf
+++ terraform/modules/k8s/auth/variables.tf
@@ -14,41 +14,53 @@ variable "domain_name" {
 variable "oauth_client_id" {
   description = "OAuth 2 Client ID"
 }
+
 variable "oauth_client_secret" {
   description = "OAuth 2 Client Secret"
 }
+
 variable "oauth_redirect_url" {
   default     = ""
   description = "OAuth 2 Redirect URL"
 }
+
 variable "oauth_oidc_issuer_url" {
   description = "OAuth 2 JWT token issuer"
 }
+
 variable "oauth_oidc_audience" {
   description = "OAuth 2 JWT token audience (for proxying valid & signatured JWT tokens)"
 }
+
 variable "oauth_oidc_scope" {
   description = "OAuth 2 Scope"
 }
 
 # OAuth2_proxy configuration
 variable "oauth_helm_chart_version" {
-  default     = "0.14.1"
+  default     = "3.1.0"
   description = "version of oauth2 proxy helm chart"
+  type        = string
 }
+
 variable "oauth_image_repository" {
-  default     = "quay.io/pusher/oauth2_proxy"
-  description = "image repository of oauth2 proxy"
+  default     = "quay.io/oauth2-proxy/oauth2-proxy"
+  description = "Image repository of oauth2-proxy"
+  type        = string
 }
+
 variable "oauth_image_tag" {
-  default     = "v4.0.0"
-  description = "image tag of oauth2 proxy"
+  default     = "v5.1.1"
+  description = "Image tag of oauth2-proxy"
+  type        = string
 }
+
 # OAuth2_proxy cookie configuration
 variable "oauth_cookie_expire" {
   default     = "3600"
   description = "TTL for issuing cookies by oauth2_proxy"
 }
+
 variable "oauth_cookie_secret" {
   description = "Secret for issuing cookies by oauth2_proxy"
 }
