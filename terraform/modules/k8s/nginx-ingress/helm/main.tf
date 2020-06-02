1f18b160213953211c615313d7a1ef1e19c958ba
Dmitriy Karbyshev <-z>
2020-06-02 18:25:39 +0300

    [feat/k8s_1.16] update charts

--- terraform/modules/k8s/nginx-ingress/helm/main.tf
+++ terraform/modules/k8s/nginx-ingress/helm/main.tf
@@ -2,7 +2,7 @@ resource "helm_release" "nginx-ingress" {
   name      = "nginx-ingress"
   chart     = "stable/nginx-ingress"
   namespace = "kube-system"
-  version   = "0.25.1"
+  version   = "0.30.0"
   wait      = false
 
   set {
