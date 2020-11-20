c02d4c5cd1e2dcf8a6f455e35d715c5670d501cd
Dmitriy Karbyshev <-z>
2020-11-21 01:29:38 +0300

    [#4] script to change k8s default storage class

---
+++ scripts/set_default_storage_class.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+STORAGE_CLASS=$1
+
+# patch current default storage class to unset `default` status
+kubectl patch storageclass $(kubectl get storageclass  | grep default | awk '{print $1}') -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
+# set new storage class
+kubectl patch storageclass ${STORAGE_CLASS} -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+
