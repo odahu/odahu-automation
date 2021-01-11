c198f03f8e944dda39f30f4c33f86d3df13b5f96
Dmitriy Karbyshev <-z>
2021-01-11 11:30:34 +0300

    [#4] pass shellcheck

--- scripts/set_default_storage_class.sh
+++ scripts/set_default_storage_class.sh
@@ -2,7 +2,7 @@
 STORAGE_CLASS=$1
 
 # patch current default storage class to unset `default` status
-kubectl patch storageclass $(kubectl get storageclass  | grep default | awk '{print $1}') -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
+kubectl patch storageclass "$(kubectl get storageclass  | grep default | awk '{print $1}')" -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
 # set new storage class
-kubectl patch storageclass ${STORAGE_CLASS} -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+kubectl patch storageclass "${STORAGE_CLASS}" -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 
