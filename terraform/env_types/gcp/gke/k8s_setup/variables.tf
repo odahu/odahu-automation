a1b57be035693afcec82f17cf98187a5f48d0e06
Egor Sokolenko <egor_sokolenko@epam.com>
2021-05-26 11:39:01 +0400

    examples variable fixed

--- terraform/env_types/gcp/gke/k8s_setup/variables.tf
+++ terraform/env_types/gcp/gke/k8s_setup/variables.tf
@@ -436,6 +436,20 @@ variable "argo" {
 # Test
 ##################
 
+variable "examples" {
+  type = object({
+    examples_urls    = any
+    examples_version = string
+    deploy_examples  = string
+  })
+  default = {
+    examples_urls    = {}
+    examples_version = ""
+    deploy_examples  = "false"
+  }
+  description = "ODAHU Examples configuration"
+}
+
 variable "wine_data_url" {
   type        = string
   description = "Wine example data URL"
