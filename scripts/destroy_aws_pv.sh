5013d0292e7e2fe945851029482f3237a9298eef
Dmitriy Karbyshev <-z>
2021-07-11 03:57:53 +0300

    AWS: optionaly delete PV volumes

---
+++ scripts/destroy_aws_pv.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+CLUSTER_NAME=$1
+REMAIN_DRIVES=$2
+
+disk_list=$(aws ec2 describe-volumes --filters Name=tag:kubernetes.io/cluster/${CLUSTER_NAME},Values=owned | jq -r '.[]|.[]|.VolumeId')
+
+if [[ "$REMAIN_DRIVES" == "false" ]]; then
+    echo PV drives will be deleted
+    for i in $disk_list
+        do
+            echo Deleting $i
+            aws ec2 delete-volume --volume-id $i
+            echo $i deleted
+        done
+else
+    echo "Drives will remain: ${disk_list}"
+fi
