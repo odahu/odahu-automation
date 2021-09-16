22255cbc17dcc885116edc6d4538aafafa3485d3
Dmitriy Karbyshev <-z>
2021-09-16 20:41:41 +0300

    fix shell script syntax

--- scripts/destroy_gcp_pv.sh
+++ scripts/destroy_gcp_pv.sh
@@ -5,7 +5,7 @@ set -e
 CLUSTER_NAME=$1
 REMAIN_DRIVES=$2
 
-disk_list=$(gcloud compute disks list --filter="name~${CLUSTER_NAME}" --format="value(selfLink.basename())")
+disk_list=$(gcloud compute disks list --filter=name~"${CLUSTER_NAME}" --format="value(selfLink.basename())")
 
 if [[ "$REMAIN_DRIVES" == "false" ]]; then
     echo PV drives will be deleted
@@ -13,7 +13,7 @@ if [[ "$REMAIN_DRIVES" == "false" ]]; then
         do
             echo "Deleting $i"
             DRIVE_LOCATION=$(gcloud compute disks list --filter="name:${i}" --format="value(location())")
-            gcloud compute disks delete "$i" --zone=${DRIVE_LOCATION} --quiet
+            gcloud compute disks delete "$i" --zone="${DRIVE_LOCATION}" --quiet
             echo "$i deleted"
         done
 else
