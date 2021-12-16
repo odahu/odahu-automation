2401b0f24dd216d794eda96dcf8189d7971ac72b
oleh_mykolaishyn <owlleg6@gmail.com>
2021-12-16 16:50:17 +0200

    611-refactor-toolchain

--- terraform/env_types/aws/eks/k8s_setup/main.tf
+++ terraform/env_types/aws/eks/k8s_setup/main.tf
@@ -369,7 +369,7 @@ module "odahuflow_helm" {
   docker_password          = var.docker_password
   odahuflow_version        = var.odahuflow_version
   packager_version         = var.packager_version
-  mlflow_toolchain_version = var.mlflow_toolchain_version
+  mlflow_trainer_integration_version = var.mlflow_trainer_integration_version
   odahu_ui_version         = var.odahu_ui_version
 
   node_pools = var.node_pools
